14c0c0c622757b0656acf8000697d8055659d42a
Davi Feliciano <dfeliciano37@gmail.com>
2021-02-16 22:15:49 -0300

    Minor Changes

--- examples/determinant.c
+++ examples/determinant.c
@@ -6,7 +6,7 @@
 int main(int argc, char **argv)
 { /* Calculate the determinant of a matrix given by csv file and time spent computing 
     Filename is given as the first cl argument */
-    if (argc > 2)
+    if (argc != 2)
     {
         fprintf(stderr, "Usage: %s [FILE]\n", argv[0]);
         exit(EXIT_FAILURE);
@@ -20,7 +20,8 @@ int main(int argc, char **argv)
     printf("Determinant = %.3f\n", det(a));
     clock_t toc = clock();
 
+    printf("Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+
     free_matrix(&a);
-    fprintf(stderr, "Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
     return 0;
 }
